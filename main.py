91c2d3243575791f460c09f0002f2d561a05aa73
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2023-12-20 20:53:42 +0000

    Randomize all knobs

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 import os
+import random
 
 INIT_PROGRAM_FILE = './resources/Init_Program.prog_bin'
 BIN_OUT_FILE = './out/Prog_000.prog_bin'
 PROGRAM_NAME = 'Logieminu 1'
 LINE_LENGTH = 32
+
+ANY_BYTE = list(range(0, 0xFF))
+FIDDLEABLE_BYTES = {
+    0x14: ANY_BYTE,
+    0x15: ANY_BYTE,
+    0x16: ANY_BYTE,
+    0x17: ANY_BYTE,
+    0x18: ANY_BYTE,
+    0x19: ANY_BYTE,
+    0x1A: ANY_BYTE,
+    0x1B: ANY_BYTE,
+    0x1C: ANY_BYTE,
+    0x1D: ANY_BYTE,
+    0x1E: ANY_BYTE,
+    0x1F: ANY_BYTE,
+    0x22: ANY_BYTE,
+    0x23: ANY_BYTE,
+    0x24: ANY_BYTE,
+    0x25: ANY_BYTE,
+    0x26: ANY_BYTE,
+    0x27: ANY_BYTE,
+    0x28: ANY_BYTE,
+    0x29: ANY_BYTE,
+    0x2A: ANY_BYTE,
+    0x2B: ANY_BYTE,
+    0x31: ANY_BYTE,
+    0x32: ANY_BYTE,
+    0x33: ANY_BYTE,
+}
 
 
 def read_init_program():
@@ -15,6 +45,10 @@ def modify_program(program):
     # Set the program name at position 0x00000004, fixed length 16
     program_name_bytes = '{:16.16}'.format(PROGRAM_NAME).encode('utf-8')
     program[4:len(program_name_bytes) + 4] = program_name_bytes
+
+    # Randomize all known program parameters
+    for offset, allowed_values in FIDDLEABLE_BYTES.items():
+        program[offset] = random.choice(allowed_values)
 
 
 def write_program(program):
